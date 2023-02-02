6cafcad2bdf9b55af6f93336d30fa8bd91487e3f
tejoumayma <oumaymamalak.55@gmail.com>
2023-02-02 19:31:54 +0100

    first commit

--- src/profile/Profile.jsx
+++ src/profile/Profile.jsx
@@ -2,15 +2,62 @@ import React from "react";
 import PropTypes from "prop-types";
 const Profile = (props) => {
   const { fullName, bio, profession, children } = props;
+  const showfullName = (fullName) => alert(`fullname: ${fullName}`);
+  const handleClick = (e) => showfullName(fullName);
   return (
-    <div>
-      <h2>Name :</h2>
-      <p>{fullName}</p>
-      <h2>Profession :</h2>
-      <p> {profession}</p>
-      <h2>Biography :</h2>
+    <div
+      className="card"
+      style={{
+        background: "#DFE7FF",
+        boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+        maxWidth: " 300px",
+        margin: "auto",
+        textalign: "center",
+      }}
+    >
+      <h2
+        style={{
+          color: "#BA49FF",
+          textAlign: "center",
+          textDecoration: "underline",
+        }}
+      >
+        Name :
+      </h2>
+      <h4>{fullName}</h4>
+      <h2
+        style={{
+          color: "#BA49FF",
+          textAlign: "center",
+          textDecoration: "underline",
+        }}
+      >
+        Profession :
+      </h2>
+      <h4> {profession} </h4>
+      <h2
+        style={{
+          color: "#BA49FF",
+          textAlign: "center",
+          textDecoration: "underline",
+        }}
+      >
+        Biography :
+      </h2>
       <p> {bio}</p>
-      {children}
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        {children}{" "}
+      </div>
+      <br />
+      <button>
+        <a href="#!" onClick={handleClick} style={{ color: "black" }}>
+          fullName
+        </a>
+      </button>
     </div>
   );
 };
