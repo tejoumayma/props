add6a5c62142765bae10d4ce068fbf04a02070b7
tejoumayma <oumaymamalak.55@gmail.com>
2023-02-03 09:23:22 +0100

    first

--- src/profile/Profile.jsx
+++ src/profile/Profile.jsx
@@ -6,7 +6,6 @@ const Profile = (props) => {
   const handleClick = (e) => showfullName(fullName);
   return (
     <div
-      className="card"
       style={{
         background: "#DFE7FF",
         boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.2)",
